bfe681d3c3af0db8026f249a1e52c25cf43e6a34
jacoposergio <djago32@gmail.com>
2022-06-10 19:46:35 +0200

    snack3

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,23 +1,51 @@
-// - Il software deve chiedere per 5 volte all’utente di inserire un numero.
-// Il programma stampa la somma di tutti i numeri inseriti.
-// Esegui questo programma in due versioni, con il for e con il while.
+// TODO  - Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// TODO  Il programma stampa la somma di tutti i numeri inseriti.
+ // TODO Esegui questo programma in due versioni, con il for e con il while.
 
 
-//creo l'array vuoto
+// !creo l'array vuoto
+// let numbers = [];
+
+// ! chiedo 5 volte all'utente di inserire un numero
+// for(let i = 0; i < 5; i++)
+//    numbers.push( parseInt( prompt("Inserisci un numero")));
+//    console.log(numbers);
+  
+// !    creo la variabile sumNumber e la sommo a tutti i numeri
+// !    dell'array
+
+//  let sumNumber = 0
+//    for(let i = 0; i < numbers.length; i++){
+   
+//        sumNumber += ( parseInt(numbers[i]));
+//     }
+    
+    // ! stampo la somma
+    // console.log("somma",sumNumber);
+
+// TODO ////////////////// versione col ciclo while ///////////
+// TODO /////////////////////////////////////////////////////////
+
+// !creo l'array vuoto
 let numbers = [];
 
-// chiedo 5 volte all'utente di inserie un numero
-for(let i = 0; i < 5; i++)
-   numbers.push( parseInt( prompt("Inserisci un numero")));
-   console.log(numbers);
-  
-//    creo la variabile sumNumber e la sommo a tutti i numeri
-//    dell'array
+// ! chiedo 5 volte i numeri all'utente
+while(numbers.length < 5) {
+    let userNumbers = prompt('Dimmi un numero');
 
-   let sumNumber = 0
-   for(let i = 0; i < numbers.length; i++){
-   
-       sumNumber += ( parseInt(numbers[i]));
-    }
-    
-    console.log("somma",sumNumber);
+   // ! pusho i numeri dati dall'utente nell'array  
+    numbers.push(userNumbers);
+
+}
+    console.log(numbers);
+
+
+// ! impost sum = 0 e lo sommo a tutti i numeri dell'array
+  let i = numbers.length;
+  let sum = 0;
+  while (i--) {
+    sum += ( parseInt(numbers[i]));
+  }
+
+//!   stampo il risultato 
+ console.log("somma",sum);
