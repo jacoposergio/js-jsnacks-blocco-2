30f1d51ee8885315ee5f2eb0e0e73dcbe4cc19fe
jacoposergio <djago32@gmail.com>
2022-06-10 17:48:44 +0200

    somma con for fatta

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,3 +1,23 @@
 // - Il software deve chiedere per 5 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 // Esegui questo programma in due versioni, con il for e con il while.
+
+
+//creo l'array vuoto
+let numbers = [];
+
+// chiedo 5 volte all'utente di inserie un numero
+for(let i = 0; i < 5; i++)
+   numbers.push( parseInt( prompt("Inserisci un numero")));
+   console.log(numbers);
+  
+//    creo la variabile sumNumber e la sommo a tutti i numeri
+//    dell'array
+
+   let sumNumber = 0
+   for(let i = 0; i < numbers.length; i++){
+   
+       sumNumber += ( parseInt(numbers[i]));
+    }
+    
+    console.log("somma",sumNumber);
